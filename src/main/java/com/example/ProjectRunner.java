b48324674c677123e33a558c9a6754bda3897648
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2022-01-26 19:02:16 +0100

    fix bad import

--- src/main/java/com/example/ProjectRunner.java
+++ src/main/java/com/example/ProjectRunner.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class ProjectRunner {
     public static void main(String args[]) {
-        Greeting.sayHi();
+        main.java.com.example.Greeting.sayHi();
     }
 }
