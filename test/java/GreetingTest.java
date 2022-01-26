f8b262aa7e3b6f3c7da9119087c5c90e5f73a3eb
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2022-01-26 18:58:50 +0100

    add example project

---
+++ test/java/GreetingTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+import static org.junit.Assert.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class GreetingTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void insertIsTrueWhenElementIsNotInTree() {
+        assertTrue(true);
+    }
+}
