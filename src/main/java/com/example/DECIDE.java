2047e92e31a62d1965aa1c03690ede23e6870805
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2022-01-28 14:40:29 +0100

    restructure proejct with multiple build files

--- src/main/java/com/example/DECIDE.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class DECIDE {
-    public static void main(String args[]) {
-        main.java.com.example.Greeting.sayHi();
-    }
-}
---
+++ src/main/java/com/example/cmdline/DECIDE.java
@@ -0,0 +1,9 @@
+package com.example.cmdline;
+
+import com.example.greeter.Greeting;
+
+public class DECIDE {
+    public static void main(String args[]) {
+        Greeting.sayHi();
+    }
+}
