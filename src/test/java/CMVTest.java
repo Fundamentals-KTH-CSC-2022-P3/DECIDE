a54499fc5dc27b0c7ade5cdfe6305d9cb2f93f1a
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-01-29 00:22:31 +0100

    Issue #3
    Create a class called CMV to store the result from each LIC.
    Furthermore, add a test file for the CMV class.
    Lastly, add all the necessary classes needed by the CMV
    class which are Parameters and Point. In the future the CMV
    class might need more classes such as a Line class and a Circle class.

---
+++ src/test/java/CMVTest.java
@@ -0,0 +1,99 @@
+package com.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CMVTest {
+
+    @Test
+    @DisplayName("LIC 0")
+    void lic0Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 1")
+    void lic1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 2")
+    void lic2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 3")
+    void lic3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 4")
+    void lic4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 5")
+    void lic5Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 6")
+    void lic6Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 7")
+    void lic7Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 8")
+    void lic8Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 9")
+    void lic9Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 10")
+    void lic10Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 11")
+    void lic11Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 12")
+    void lic12Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 13")
+    void lic13Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC 14")
+    void lic14Test() {
+        assertTrue(true);
+    }
+}
