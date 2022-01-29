74587f98ba778e5e790e0f3d9fe5dcf554a25f7f
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-01-29 17:48:58 +0100

    add comments to the Parameters class

--- src/main/java/com/example/core/Parameters.java
+++ src/main/java/com/example/core/Parameters.java
@@ -4,23 +4,23 @@ package com.example.core;
  * Represents a set of different parameters that are used when implementing each LIC.
  */
 public class Parameters {
-    public double LENGTH1;
-    public double RADIUS1;
-    public double EPSILON;
-    public double AREA1;
-    public int Q_PTS;
-    public int QUADS;
-    public double DIST;
-    public int N_PTS;
-    public int K_PTS;
-    public int A_PTS;
-    public int B_PTS;
-    public int C_PTS;
-    public int D_PTS;
-    public int E_PTS;
-    public int F_PTS;
-    public int G_PTS;
-    public double LENGTH2;
-    public double RADIUS2;
-    public double AREA2;
+    public double LENGTH1;      // Length in LICs 0, 7, 12
+    public double RADIUS1;      // Radius in LICs 1, 8, 13
+    public double EPSILON;      // Deviation from PI in LICs 2, 9
+    public double AREA1;        // Area in LICs 3, 10, 14
+    public int Q_PTS;           // Number of consecutive points in LIC 4
+    public int QUADS;           // Number of quadrants in LIC 4
+    public double DIST;         // Distance in LIC 6
+    public int N_PTS;           // Number of consecutive points in LIC 6
+    public int K_PTS;           // Number of intervening points in LICs 7, 12
+    public int A_PTS;           // Number of intervening points in LICs 8, 13
+    public int B_PTS;           // Number of intervening points in LICs 8, 13
+    public int C_PTS;           // Number of intervening points in LICs 9
+    public int D_PTS;           // Number of intervening points in LICs 9
+    public int E_PTS;           // Number of intervening points in LICs 10, 14
+    public int F_PTS;           // Number of intervening points in LICs 10, 14
+    public int G_PTS;           // Number of intervening points in LICs 11
+    public double LENGTH2;      // Maximum length in LIC 12
+    public double RADIUS2;      // Maximum radius in LIC 13
+    public double AREA2;        // Maximum area in LIC 14
 }
