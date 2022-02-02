b8491da53ad2e7591f8fa01641dafdc9abb9c1b0
Arvid Siberov <asiberov@kth.se>
2022-02-02 09:59:47 +0100

    Rewrite verification method to be static.
    The `Parameters.verify()` needed access to the number of points. This
    commit solves that by passing the `points` array as an argument to the
    `verify`-method, instead of requiring NUMPOINTS to be a field in the
    `Parameters` class.
    
    The test cases have been rewritten to reflect the changes, and have also
    gotten better documentation.

--- src/main/java/com/example/core/Parameters.java
+++ src/main/java/com/example/core/Parameters.java
@@ -23,121 +23,120 @@ public class Parameters {
     public double LENGTH2;      // Maximum length in LIC 12
     public double RADIUS2;      // Maximum radius in LIC 13
     public double AREA2;        // Maximum area in LIC 14
-    public int NUMPOINTS;       // Total number of input points
 
     /**
      * Verifies that all properties are conforming to the requirements specification (RS).
      * @throws Exception if any property is invalid.
      */
-    public void verify() {
+    public static void verify(Parameters parameters, Point[] points) {
 
         // According to RS 2.1.0
-        if (LENGTH1 < 0) {
+        if (parameters.LENGTH1 < 0) {
             throw new IllegalArgumentException("LENGTH1 must be non-negative.");
         }
 
         // According to RS 2.1.1
-        if (RADIUS1 < 0) {
+        if (parameters.RADIUS1 < 0) {
             throw new IllegalArgumentException("RADIUS1 must be non-negative.");
         }
 
         // According to RS 2.1.2
-        if (EPSILON < 0 || EPSILON >= Math.PI) {
+        if (parameters.EPSILON < 0 || parameters.EPSILON >= Math.PI) {
             throw new IllegalArgumentException("EPSILON must be in the range [0, pi)");
         }
 
         // According to RS 2.1.3
-        if (AREA1 < 0) {
+        if (parameters.AREA1 < 0) {
             throw new IllegalArgumentException("AREA must be non-negative");
         }
 
         // According to RS 2.1.4
-        if (Q_PTS < 2 || Q_PTS > NUMPOINTS) {
+        if (parameters.Q_PTS < 2 || parameters.Q_PTS > points.length) {
             throw new IllegalArgumentException("Q_PTS must be in the range [2, NUMPOINTS]");
         }
 
         // According to RS 2.1.4
-        if (QUADS < 1 || QUADS > 3) {
+        if (parameters.QUADS < 1 || parameters.QUADS > 3) {
             throw new IllegalArgumentException("QUADS must be in the range [1, 3]");
         }
 
         // According to RS 2.1.6
-        if (N_PTS < 3 || N_PTS > NUMPOINTS) {
+        if (parameters.N_PTS < 3 || parameters.N_PTS > points.length) {
             throw new IllegalArgumentException("N_PTS must be in the range [3, NUMPOINTS]");
         }
 
         // According to RS 2.1.6
-        if (DIST < 0) {
+        if (parameters.DIST < 0) {
             throw new IllegalArgumentException("DIST must be non-negative");
         }
 
         // According to RS 2.1.7
-        if (K_PTS < 1 || K_PTS > NUMPOINTS - 2) {
+        if (parameters.K_PTS < 1 || parameters.K_PTS > points.length - 2) {
             throw new IllegalArgumentException("K_PTS must be in the range [1, NUMPOINTS-2]");
         }
 
         // According to RS 2.1.8
-        if (A_PTS < 1) {
+        if (parameters.A_PTS < 1) {
             throw new IllegalArgumentException("A_PTS must be positive");
         }
 
         // According to RS 2.1.8
-        if (B_PTS < 1) {
+        if (parameters.B_PTS < 1) {
             throw new IllegalArgumentException("B_PTS must be positive");
         }
 
         // According to RS 2.1.8
-        if (A_PTS + B_PTS > NUMPOINTS - 3) {
+        if (parameters.A_PTS + parameters.B_PTS > points.length - 3) {
             throw new IllegalArgumentException("A_PTS + B_PTS must not be greater than NUMPOINTS - 3");
         }
 
         // According to RS 2.1.9
-        if (C_PTS < 1) {
+        if (parameters.C_PTS < 1) {
             throw new IllegalArgumentException("C_PTS must be positive");
         }
 
         // According to RS 2.1.9
-        if (D_PTS < 1) {
+        if (parameters.D_PTS < 1) {
             throw new IllegalArgumentException("D_PTS must be positive");
         }
 
         // According to RS 2.1.9
-        if (C_PTS + D_PTS > NUMPOINTS - 3) {
+        if (parameters.C_PTS + parameters.D_PTS > points.length - 3) {
             throw new IllegalArgumentException("C_PTS + D_PTS must not be greater than NUMPOINTS - 3");
         }
 
         // According to RS 2.1.10
-        if (E_PTS < 1) {
+        if (parameters.E_PTS < 1) {
             throw new IllegalArgumentException("E_PTS must be positive");
         }
 
         // According to RS 2.1.10
-        if (F_PTS < 1) {
+        if (parameters.F_PTS < 1) {
             throw new IllegalArgumentException("F_PTS must be positive");
         }
 
         // According to RS 2.1.10
-        if (E_PTS + F_PTS > NUMPOINTS - 3) {
+        if (parameters.E_PTS + parameters.F_PTS > points.length - 3) {
             throw new IllegalArgumentException("E_PTS + F_PTS must not be greater than NUMPOINTS - 3");
         }
 
         // According to RS 2.1.11
-        if (G_PTS < 1 || G_PTS > NUMPOINTS - 2) {
+        if (parameters.G_PTS < 1 || parameters.G_PTS > points.length - 2) {
             throw new IllegalArgumentException("G_PTS must be in the range [1, NUMPOINTS - 2]");
         }
 
         // According to RS 2.1.12
-        if (LENGTH2 < 0) {
+        if (parameters.LENGTH2 < 0) {
             throw new IllegalArgumentException("LENGTH2 must be non-negative");
         }
 
         // According to RS 2.1.13
-        if (RADIUS2 < 0) {
+        if (parameters.RADIUS2 < 0) {
             throw new IllegalArgumentException("RADIUS2 must be non-negative");
         }
 
         // According to RS 2.1.14
-        if (AREA2 < 0) {
+        if (parameters.AREA2 < 0) {
             throw new IllegalArgumentException("AREA2 must be non-negative");
         }
     }
--- src/test/java/ParametersTest.java
+++ src/test/java/ParametersTest.java
@@ -1,14 +1,22 @@
 package com.example;
 
 import com.example.core.Parameters;
+import com.example.core.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParametersTest {
 
+    /**
+     * Test that we can create and validate parameters and points that adhere to the specification.
+     * There are many constraints on the different parameters, so refer to the requirements specification for
+     * explanation of the different numeric values.
+     */
     @Test
     @DisplayName("Valid parameters")
     void validParametersTest() {
@@ -33,17 +41,28 @@ public class ParametersTest {
         validParams.LENGTH2 = 1.0;
         validParams.RADIUS2 = 1.0;
         validParams.AREA2 = 1.0;
-        validParams.NUMPOINTS = 5;
 
-        assertDoesNotThrow(validParams::verify);
+        // Create arbitrary points array. 10 points should be valid.
+        com.example.core.Point[] points = new Point[10];
+        Arrays.fill(points, new Point(0.0, 0.0));
+
+        assertDoesNotThrow(() -> Parameters.verify(validParams, points));
     }
 
+    /**
+     * Test that invalid parameters lead to an exception being thrown. The parameters created fail to fulfil the
+     * requirements stated in the requirements specification.
+     */
     @Test
     @DisplayName("Invalid parameters")
     void invalidParametersTest() {
-        Parameters params = new Parameters();
         // This should fail to verify because of several reasons. For instance,
         // N_PTS must be at least 3, and it will be set to 0.
-        assertThrows(IllegalArgumentException.class, params::verify);
+        Parameters invalidParams = new Parameters();
+
+        // Create a point just so we have something non-null to pass along to verify
+        Point[] points = new Point[1];
+        points[0] = new Point(0.0, 0.0);
+        assertThrows(IllegalArgumentException.class, () -> Parameters.verify(invalidParams, points));
     }
 }
